706b11189a55d4f02e825cb43bedb5774ed48d2a
FGM-148 <maciej@siekierda@gmail.com>
2018-09-18 22:43:15 +0200

    Add Spring Security
    Simple security with users and roles

---
+++ src/main/java/com/ms/et/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.ms.et.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/item/show").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/item/new").hasAnyRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+
+        http.csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        authenticationMgr.inMemoryAuthentication()
+                .withUser("user").password(encoder.encode("ti2018"))
+                .authorities("ROLE_USER").and()
+                .withUser("admin").password(encoder.encode("ti2018"))
+                .authorities("ROLE_USER", "ROLE_ADMIN");
+    }
+
+}
